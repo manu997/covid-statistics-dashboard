d18acfca2723d7474f6ffd711017f51c5e76b162
mmorenoa <manuelmorenoasiain@gmail.com>
2024-07-27 17:29:20 +0200

    Translations added with i18n-react

--- src/App.tsx
+++ src/App.tsx
@@ -12,37 +12,37 @@ import Card from './components/Card';
 import Table from './components/Table';
 import { ColumnsDef } from './components/Table/Table';
 import ReactCountryFlag from 'react-country-flag';
+import { Trans, useTranslation } from 'react-i18next';
 
 function App() {
+  const [enableQuery, setEnableQuery] = useState(false);
+  const [name, setName] = useState('');
+
   const form = useForm<FormData>({
     resolver: zodResolver(FormSchema),
   });
-  const regionNames = useMemo(
-    () => new Intl.DisplayNames(['es'], { type: 'region' }),
-    []
-  );
-  const [enableQuery, setEnableQuery] = useState(false);
-  const [name, setName] = useState('');
 
   const { data, isLoading, isFetched, success, errors } = useGetData({
     name,
     enableQuery,
   });
 
+  const { t, i18n } = useTranslation();
+
   const onSubmit = useCallback(async ({ name }: FormData) => {
     setName(name);
     setEnableQuery(true);
   }, []);
 
-  useEffect(() => {
-    isFetched && setEnableQuery(false);
-  }, [isFetched]);
-
+  const regionNames = useMemo(
+    () => new Intl.DisplayNames([i18n.language], { type: 'region' }),
+    [i18n.language]
+  );
   const columnsDef: ColumnsDef<Country> = useMemo(
     () => [
       {
         accessor: 'country_id',
-        header: 'País',
+        header: t('COUNTRY'),
         cell: (info) => (
           <div className='flag-item'>
             <ReactCountryFlag countryCode={info} svg />
@@ -53,49 +53,56 @@ function App() {
       },
       {
         accessor: 'probability',
-        header: 'Probabilidad',
+        header: t('PROBABILITY'),
         cell: (info) => <span>{Math.trunc(info * 100)}%</span>,
         align: 'right',
         size: 50,
       },
     ],
-    [regionNames]
+    [regionNames, t]
   );
 
-  const cardTitle = `El nombre de ${data.genderize?.name} tiene las siguientes estadísticas:`;
+  useEffect(() => {
+    isFetched && setEnableQuery(false);
+  }, [isFetched]);
 
   return (
-    <div className='container'>
-      <Form form={form} onSubmit={onSubmit}>
-        <FormItem label='Nombre'>
-          <Input type='text' placeholder='Introduce tu nombre' name='name' />
-        </FormItem>
-        <button type='submit' disabled={isLoading}>
-          {isLoading ? <span className='loader' /> : 'Generar estadísticas'}
-        </button>
-      </Form>
-      {success && (
-        <Card title={cardTitle}>
-          <p>
-            Su género es <strong>{data.genderize?.gender}</strong> con un{' '}
-            <strong>{data.genderize?.probability}%</strong> de probabilidad.
-          </p>
-          <p>
-            Su edad es de <strong>{data.agify?.age} años</strong>.
-          </p>
-          <p>Probabilidad de su país sea...</p>
-          <Table columns={columnsDef} data={data.nationalize?.country || []} />
-        </Card>
-      )}
-      {errors?.map(
-        (error) =>
-          error && (
-            <span key={error.message} className='error-message'>
-              {error?.message}
-            </span>
-          )
-      )}
-    </div>
+    <Trans t={t}>
+      <div className='container'>
+        <Form form={form} onSubmit={onSubmit}>
+          <FormItem label={t('NAME')}>
+            <Input type='text' placeholder={t('INTRODUCE_NAME')} name='name' />
+          </FormItem>
+          <button type='submit' disabled={isLoading}>
+            {isLoading ? <span className='loader' /> : t('GENERATE_STATITICS')}
+          </button>
+        </Form>
+        {success && (
+          <Card title={t('DATA_TITLE', { name: data.genderize?.name })}>
+            <p>
+              {t('DATA_GENDERIZE', {
+                gender: t(data.genderize?.gender.toUpperCase()).toLowerCase(),
+                probability: data.genderize?.probability * 100,
+              })}
+            </p>
+            <p>{t('DATA_AGIFY', { age: data.agify?.age })}</p>
+            <p>{t('DATA_NATIONALIZE_TITLE')}</p>
+            <Table
+              columns={columnsDef}
+              data={data.nationalize?.country || []}
+            />
+          </Card>
+        )}
+        {errors?.map(
+          (error) =>
+            error && (
+              <span key={error.message} className='error-message'>
+                {error?.message}
+              </span>
+            )
+        )}
+      </div>
+    </Trans>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './Main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './i18n';
 
 const queryClient = new QueryClient();
 
